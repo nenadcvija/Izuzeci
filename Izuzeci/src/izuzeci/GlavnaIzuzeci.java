a17478a595038ce3a814a7fd205e0387ece5b878
okkursevi1 <okkursevi1@UC5PC10>
2019-11-29 21:49:11 +0100

    izuzeci

---
+++ Izuzeci/src/izuzeci/GlavnaIzuzeci.java
@@ -0,0 +1,47 @@
+package izuzeci;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GlavnaIzuzeci {
+
+	public static void main(String[] args) {
+			
+		Metode m = new Metode();
+		
+		try {
+			System.out.println(m.podeli(10, 0));
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		/*
+		 * int a = 10; int b = 2;
+		 * 
+		 * List<String> nekaLista = new ArrayList<String>();
+		 * 
+		 * try{ System.out.println(a/b); System.out.println("Radi!!!!!!");
+		 * nekaLista.get(5);
+		 * 
+		 * } catch(ArithmeticException e) {
+		 * System.out.println("Ovde je deljenje sa nulom!"); }catch
+		 * (ArrayIndexOutOfBoundsException e) {
+		 * System.out.println("Izasao si iz opsega!"); }catch(Exception e) {
+		 * System.out.println("ovo je za sve"); }
+		 */
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ Izuzeci/src/izuzeci/Metode.java
@@ -0,0 +1,19 @@
+package izuzeci;
+
+public class Metode {
+	
+	
+	public double podeli (double a, int b) throws Exception{
+		
+			return a/b;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
